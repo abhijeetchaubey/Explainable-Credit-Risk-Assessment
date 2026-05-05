312896773475735d530056782d76162b3345fbeb
abhijeetchaubey <chaubeyabhijeet36@gmail.com>
2026-05-05 20:22:23 +0530

    fix: update API_BASE_URL to use the correct production endpoint

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API_BASE_URL =  'http://127.0.0.1:8000';
+const API_BASE_URL =  'https://explainable-credit-risk-assessment-1.onrender.com/';
+const local_host_url ='http://127.0.0.1:8000/'
+
 
 export const assessRisk = async (applicationData) => {
   try {
